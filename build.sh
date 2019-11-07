09878247df0352e7dfedae7d45759434712fb4dc
Ron Cemer <ron@roncemer.com>
2019-11-06 22:29:39 -0600

    Revert to non-cli codebase.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# yarn install
-# yarn global add rimraf webpack webpack-cli
-
-npm run build
-
-# No:
-#vue serve --open src/AjaxCombobox.vue
--- src/plugin.js
+++
@@ -1,7 +0,0 @@
-import AjaxCombobox from './AjaxCombobox.vue';
-
-module.exports = {
-  install: function (Vue, options) {
-    Vue.component('ajax-combobox', AjaxCombobox);
-  }
-};
